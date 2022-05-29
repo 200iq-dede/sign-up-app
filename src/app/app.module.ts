d7344f6c93c6dfe36e04c33c7c9c5b1ef1ab3515
Dede <winsandymyint@gmail.com>
2022-05-29 09:09:01 +0630

    Start-up the angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserCreateComponent } from './components/user-create/user-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserCreateComponent
   ],
   imports: [
     BrowserModule,

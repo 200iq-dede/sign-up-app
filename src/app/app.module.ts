0e89c75eeccbc02d44ea1dd4143e466e716b6019
Dede <winsandymyint@gmail.com>
2022-05-29 21:16:24 +0630

    Add upload icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { UserCreateComponent } from './components/user-create/user-create.component';
